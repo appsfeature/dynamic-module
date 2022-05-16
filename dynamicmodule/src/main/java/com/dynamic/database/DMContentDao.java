859c06702b25af079842f777df265729d799fe19
droidhelios <droidhelios@gmail.com>
2022-05-16 12:37:16 +0530

    Delete old records by catId2

--- dynamicmodule/src/main/java/com/dynamic/database/DMContentDao.java
+++ dynamicmodule/src/main/java/com/dynamic/database/DMContentDao.java
@@ -61,7 +61,7 @@ public interface DMContentDao {
     @Query("DELETE FROM dm_content WHERE id ==:id")
     void delete(int id);
 
-    @Query("DELETE FROM dm_content WHERE cat_id ==:catId")
+    @Query("DELETE FROM dm_content WHERE sub_cat_id ==:catId")
     void deleteContentsByCatId(int catId);
 
     @Query("DELETE FROM dm_content")
