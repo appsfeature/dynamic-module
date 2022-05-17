be1aa0c6e90a109c9f065ae97ace517195617ee9
droidhelios <droidhelios@gmail.com>
2022-05-17 11:24:26 +0530

    Udate Constants

--- dynamicmodule/src/main/java/com/dynamic/util/DMConstants.java
+++ dynamicmodule/src/main/java/com/dynamic/util/DMConstants.java
@@ -6,6 +6,7 @@ public interface DMConstants {
 
     String DEFAULT_BASE_URL = "http://appsfeature.com/apps/api/v1/database/";
     String DEFAULT_BASE_IMAGE_URL = "http://appsfeature.com/apps/public/uploads/images/";
+    String DEFAULT_BASE_PDF_URL = "http://appsfeature.com/apps/public/uploads/pdf/";
 
     String TITLE = "title";
     String CAT_ID = "cat_id";
